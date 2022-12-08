32a10f63227337487e1ce41579c8f667499621df
gracekells <graceksd16@gmail.com>
2022-12-08 08:54:53 +0700

    update barang

--- p25/src/Barang.java
+++ p25/src/Barang.java
@@ -1,12 +1,76 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author u-mdp
- */
+//import javax.swing.Default
+        
+import javax.swing.table.DefaultTableModel;
 public class Barang {
+    private String kode;
+    private String nama;
+    private String jenis;
+    private String kondisi;
+    private int harga;
+    private int jumlah;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getKondisi() {
+        return kondisi;
+    }
+
+    public void setKondisi(String kondisi) {
+        this.kondisi = kondisi;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+    public DefaultTableModel tampilBarang(){
+        //nama kolom
+        String[] kolom = {
+            "Kode",
+            "Nama",
+            "Jenis",
+            "Kondisi",
+            "Harga",
+            "Jumlah"
+        };
+        
+        DefaultTableModel model = new DefaultTableModel(null, kolom);
+        return model;
+    }
+    
     
 }
